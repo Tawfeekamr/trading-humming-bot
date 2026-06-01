74476b882c9e93d7618d345fdddeb458d8ed21c2
tawfeekamr <totostyle@hotmail.com>
2026-06-02 00:39:07 +0400

    chore: update build artifacts and dependencies in trading-engine-core

--- trading-engine-core/target/debug/deps/unicode_ident-24076b0999028426.d
+++
@@ -1,8 +0,0 @@
-/Users/amro/WebstormProjects/trading-humming-bot/trading-engine-core/target/debug/deps/unicode_ident-24076b0999028426.d: /Users/amro/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/amro/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/amro/WebstormProjects/trading-humming-bot/trading-engine-core/target/debug/deps/libunicode_ident-24076b0999028426.rlib: /Users/amro/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/amro/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/amro/WebstormProjects/trading-humming-bot/trading-engine-core/target/debug/deps/libunicode_ident-24076b0999028426.rmeta: /Users/amro/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /Users/amro/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/Users/amro/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs:
-/Users/amro/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs:
